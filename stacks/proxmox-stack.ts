60d84648a1c17c54e9d298ff442f62a7ea96463b
ashubisht <19997964+ashubisht@users.noreply.github.com>
2025-11-22 16:04:19 +0530

    Update dependency to fix hang issues

--- stacks/proxmox-stack.ts
+++ stacks/proxmox-stack.ts
@@ -30,24 +30,32 @@ export class ProxmoxStack extends TerraformStack {
       pmApiUrl: config.proxmoxUrl,
       pmApiTokenId: config.proxmoxTokenId,
       pmApiTokenSecret: config.proxmoxTokenSecret,
+      pmTlsInsecure: true,
     });
 
     new VmQemu(this, "vm", {
       name: config.vmName,
       targetNode: config.proxmoxNode,
       vmid: config.vmId,
-      iso: `${config.isoStorage}:iso/${config.isoImage}`,
       osType: config.osType,
       agent: 1,
       scsihw: "virtio-scsi-pci",
       disk: [{
+        slot: "ide0",
+        iso: `${config.isoStorage}:iso/${config.isoImage}`,
+        type: "cdrom",
+      }, {
+        slot: "scsi0",
         storage: config.diskStorage,
         size: `${config.diskSize}G`,
-        type: "scsi",
+        type: "disk",
       }],
       memory: config.memory,
-      cores: config.cores,
+      cpu: {
+        cores: config.cores,
+      },
       network: [{
+        id: 0,
         bridge: config.networkBridge,
         model: config.networkModel,
       }],
